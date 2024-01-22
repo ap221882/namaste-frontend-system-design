e7933182b4bb108e0c4df6f16141d9007da036ca
Ajay Pathak <ajay.pathak@uniblox.io>
2024-01-22 08:50:59 +0530

    mitigations

--- Security/XSS/mitigation/index.js
+++ Security/XSS/mitigation/index.js
@@ -4,21 +4,23 @@ const PORT = 3010;
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader(
-        'Content-Security-Policy',
-        "default-src 'self';" + 
-        "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;"
-    );
-    next();
-})
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self';" /** load only from my domain - load something */ +
+      "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;" //~? allow from self and unsecure.com
+    //~? unsafe-inline allows inline script are allowed and can be loaded
+  );
+  //~? randomKey is the attribute nonce value to be added to script tag so that it will be allowed to execute
+  next();
+});
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log(req.url);
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started at http://locolhost:${PORT}`);
+  console.log(`Server started at http://locolhost:${PORT}`);
 });
