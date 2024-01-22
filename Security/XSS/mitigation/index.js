bb81d14b0d589c2a6ef83b375bc052df5f51082c
Ajay Pathak <ajay.pathak@uniblox.io>
2024-01-22 18:00:41 +0530

    chore: initial structure

--- Security/XSS/mitigation/index.js
+++ Security/XSS/mitigation/index.js
@@ -22,5 +22,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://locolhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
